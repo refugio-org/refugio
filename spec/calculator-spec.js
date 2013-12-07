faa63d0524868cf37d6944069409894264ffac96
Johannes Thoenes <johannes.thoenes@ottogroup.com>
2013-12-07 12:32:57 +0100

    Jasmine Test set up.

---
+++ spec/calculator-spec.js
@@ -0,0 +1,13 @@
+describe("multiplication", function () {
+  it("should multiply 2 and 3", function () {
+    // Given
+		var x1 = 2, 
+	      x2 = 3;
+	
+	  // When
+	  var product = x1 * x2;
+
+		// Then
+	  expect(product).toBe(6);
+  });
+}); 
