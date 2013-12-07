fc4db7bf4611f9c9a341d7a5252eb0e7592b0511
egal <egal@debious>
2013-12-07 19:45:42 +0100

    multilayer dropdown interaction management

--- public/js/management.js
+++ public/js/management.js
@@ -1,3 +1,15 @@
 $(function() {  
 
+$( "select[name='Types']" ).change(function() {
+  var target=$(this).val();
+  $('.type').addClass('hidden');
+	$('.actions'+target).addClass('hidden');  
+	$('.'+target).removeClass('hidden');
+});
+
+$( ".type" ).change(function() {
+	$('.actions').removeClass('hidden');
+});
+
+
 }); 
