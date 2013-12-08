0dc67abd0e3d3f8a9c1caaa6113745a234b4638f
Henry <cryptix@riseup.net>
2013-12-08 14:14:00 +0100

    tabs to spaces!!1

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ var Category = require('../models/category.js');
 
 exports.list = function(req, res) {
   Category.find({}, function(err, results) {
-	  if (err) throw err;
-	  res.render("categoryList.jade", {cats: results});
+    if (err) throw err;
+    res.render("categoryList.jade", {cats: results});
   });
 };
--- routes/shopping.js
+++ routes/shopping.js
@@ -9,18 +9,18 @@ exports.list = function(req, res) {
 }
 
 exports.listForPlace = function(req, res) {
-	Place.findOne({name: req.params.name}, function(err, foundPlace) {
-		if (err) throw err;
+  Place.findOne({name: req.params.name}, function(err, foundPlace) {
+    if (err) throw err;
 
-		if (foundPlace) {
-			Item.find({location: foundPlace._id}, function(err, items) {
-		    if (err) throw err;
-		    
-		    res.render("shoppingList.jade", {place: foundPlace, items: items});
-		  });
-		} else {
-			// place not found
-		}
-	});
+    if (foundPlace) {
+      Item.find({location: foundPlace._id}, function(err, items) {
+        if (err) throw err;
+        
+        res.render("shoppingList.jade", {place: foundPlace, items: items});
+      });
+    } else {
+      // place not found
+    }
+  });
   
 }
