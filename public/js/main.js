70efbb1d6eaf3b8f0595c40f9cd7dadc11e141cd
egal <egal@debious>
2013-12-08 15:37:19 +0100

    new cart counter

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+$(function() {  
+
+	$('.startButton').on('click', function(){
+		window.location="/"+$(this).attr('alt');
+	})
+
+	$('.list-group-item').on('click', function(){
+		$(this).addClass('hidden');
+    var counter=$('.counter'),
+    inCart=counter.attr('data-count');
+    inCart++;
+    counter.attr('data-count',inCart);
+    counter.removeClass('hidden').html(inCart);
+	})
+
+})
